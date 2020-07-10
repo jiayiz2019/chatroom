c29918b4d9ff0f2ad5c7abfd65d0f91d9221b55c
jiayiz <jiayiz2019@163.com>
2020-07-10 14:38:05 +0800

    modify

--- client/client.c
+++ client/client.c
@@ -4,23 +4,31 @@
 	> Mail: suyelu@126.com
 	> Created Time: Wed 08 Jul 2020 04:32:12 PM CST
  ************************************************************************/
-
 #include "head.h"
 
 int server_port = 0;
 char server_ip[20] = {0};
-int team = -1;
-char name[20] = {0};
-char log_msg[512] = {0};
 char *conf = "./football.conf";
 int sockfd = -1;
 
+void logout (int signum) {
+    struct ChatMsg msg;
+    msg.type = CHAT_FIN;
+    send(sockfd, (void *)&msg, sizeof(msg),0);
+    close(sockfd);
+    DBG(RED"Bye!\n"NONE);
+    exit(0);
+}
 int main(int argc, char **argv) {
     int opt;
+    struct LogRequest request;        //HERE
+    struct LogResponse response;     //HERE
+    bzero(&request, sizeof(request));
+    bzero(&response, sizeof(response));
     while ((opt = getopt(argc, argv, "h:p:t:m:n:")) != -1) {
         switch (opt) {
             case 't':
-                team = atoi(optarg);
+                request.team = atoi(optarg); //HERE
                 break;
             case 'h':
                 strcpy(server_ip, optarg);
@@ -29,27 +37,26 @@ int main(int argc, char **argv) {
                 server_port = atoi(optarg);
                 break;
             case 'm':
-                strcpy(log_msg, optarg);
+                strcpy(request.msg, optarg); //HERE
                 break;
             case 'n':
-                strcpy(name, optarg);
+                strcpy(request.name, optarg); //HERE
                 break;
             default:
                 fprintf(stderr, "Usage : %s [-hptmn]!\n", argv[0]);
                 exit(1);
         }
     }
-    
+
 
     if (!server_port) server_port = atoi(get_conf_value(conf, "SERVERPORT"));
-    if (!team) team = atoi(get_conf_value(conf, "TEAM"));
+    if (!request.team) request.team = atoi(get_conf_value(conf, "TEAM")); //HERE
     if (!strlen(server_ip)) strcpy(server_ip, get_conf_value(conf, "SERVERIP"));
-    if (!strlen(name)) strcpy(name, get_conf_value(conf, "NAME"));
-    if (!strlen(log_msg)) strcpy(log_msg, get_conf_value(conf, "LOGMSG"));
+    if (!strlen(request.name)) strcpy(request.name, get_conf_value(conf, "NAME"));//HERE
+    if (!strlen(request.msg)) strcpy(request.msg, get_conf_value(conf, "LOGMSG"));//HERE
 
 
-    DBG("<"GREEN"Conf Show"NONE"> : server_ip = %s, port = %d, team = %s, name = %s\n%s",\
-        server_ip, server_port, team ? "BLUE": "RED", name, log_msg);
+    DBG("<"GREEN"Conf Show"NONE"> : server_ip = %s, port = %d, team = %s, name = %s\n%s\n",server_ip, server_port, request.team ? "BLUE": "RED", request.name, request.msg);
 
     struct sockaddr_in server;
     server.sin_family = AF_INET;
@@ -62,8 +69,43 @@ int main(int argc, char **argv) {
         perror("socket_udp()");
         exit(1);
     }
-    
-    sendto(sockfd, log_msg, strlen(log_msg), 0, (struct sockaddr *)&server, len);
+
+    sendto(sockfd, (void *)&request, sizeof(request), 0, (struct sockaddr *)&server, len);//HERE
+
+    fd_set rfds;
+    FD_ZERO(&rfds);
+    FD_SET(sockfd, &rfds);
+    struct timeval tv;
+    tv.tv_sec = 5;
+    tv.tv_usec = 0;
+
+    int retval = select(sockfd + 1, &rfds, NULL, NULL, &tv);
+    if (retval < 0) {
+        perror("select()");
+        exit(1);                             
+    } else if (retval) {
+        int ret = recvfrom(sockfd, (void *)&response, sizeof(response), 0, (struct sockaddr *)&server, &len);
+        if (ret != sizeof(response) || response.type) {
+            DBG(RED"ERROR : "NONE"The  Server refused your login request.\n\tThis May be helpful : %s\n", response.msg);
+            exit(1);    
+        }                                                                
+    } else {
+        DBG(RED"ERROR : "NONE"The Server is out of service.\n");
+        exit(1);
+    }
+    DBG(GREEN"SERVER : "NONE" %s \n", response.msg);
+    connect(sockfd, (struct sockaddr *)&server, len);
+
+    signal(SIGINT , logout);
+    while (1) {
+        struct ChatMsg msg;
+        msg.type = CHAT_WALL;
+        printf(RED"please Input: \n"NONE);
+        scanf("%[^\n]s", msg.msg);
+        getchar();
+        send(sockfd, (void*)&msg, sizeof(msg), 0);
+    }
+
 
     return 0;
 }
